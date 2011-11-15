f9902803db79c59578162aa19766682350a4919b
Gabriel Böhme <m.gabrielboehme@googlemail.com>
2011-11-15 08:11:04 +0100

    Internationalisierung hinzugefügt - Deutsch/Englisch

---
+++ qml/i18n/dateevent_de.ts
@@ -0,0 +1,158 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../MainPage.qml" line="21"/>
+        <source>DateEvent</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="45"/>
+        <source>berücksichtigte Kalendertage:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="67"/>
+        <source>berücksichtigte Kalender:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="74"/>
+        <source>Auswahl Kalender</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="90"/>
+        <source>angezeigte Termine:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="112"/>
+        <source>Nächste Termine zuerst zeigen:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="130"/>
+        <source>Daemon starten</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="141"/>
+        <location filename="../MainPage.qml" line="145"/>
+        <location filename="../MainPage.qml" line="229"/>
+        <source>Start</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="145"/>
+        <source>manuelles Update</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="152"/>
+        <source>Kalender Auswahl:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="154"/>
+        <location filename="../MainPage.qml" line="214"/>
+        <source>Ok</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="168"/>
+        <source>Anzahl Tage:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="191"/>
+        <source>Anzahl Termine:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="212"/>
+        <source>Information</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="213"/>
+        <source>Diese Anwendung wurde von Boris Pohler und Gabriel Böhme entwickelt und sie steht unter der GPL3 Lizenz.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="223"/>
+        <source>Achtung!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="224"/>
+        <source>Möchtest du den Termin-Feed wirklich beenden?</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="225"/>
+        <source>Ja</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="226"/>
+        <source>Nein</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="239"/>
+        <source>Warnung!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="240"/>
+        <source>Im Moment ist kein Kalender ausgewählt, der angezeigt werden kann.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="241"/>
+        <source>Jetzt wählen</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="242"/>
+        <source>Abbrechen</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="9"/>
+        <source>Start</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="10"/>
+        <source>manuelles Update</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="14"/>
+        <source>Daemon stoppen</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="15"/>
+        <source>Daemon starten</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="49"/>
+        <source>Feed beenden</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="50"/>
+        <source>Information</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
---
+++ qml/i18n/dateevent_en.ts
@@ -0,0 +1,158 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en">
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../MainPage.qml" line="21"/>
+        <source>DateEvent</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="45"/>
+        <source>berücksichtigte Kalendertage:</source>
+        <translation>Included days:</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="67"/>
+        <source>berücksichtigte Kalender:</source>
+        <translation>Included calendars:</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="74"/>
+        <source>Auswahl Kalender</source>
+        <translation>Calendar selection</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="90"/>
+        <source>angezeigte Termine:</source>
+        <translation>Displaying dates:</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="112"/>
+        <source>Nächste Termine zuerst zeigen:</source>
+        <translation>Show next dates on top:</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="130"/>
+        <source>Daemon starten</source>
+        <translation>Start daemon</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="141"/>
+        <location filename="../MainPage.qml" line="145"/>
+        <location filename="../MainPage.qml" line="229"/>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="145"/>
+        <source>manuelles Update</source>
+        <translation>Update manually</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="152"/>
+        <source>Kalender Auswahl:</source>
+        <translation>Calendar selection:</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="154"/>
+        <location filename="../MainPage.qml" line="214"/>
+        <source>Ok</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="168"/>
+        <source>Anzahl Tage:</source>
+        <translation>Amount of days:</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="191"/>
+        <source>Anzahl Termine:</source>
+        <translation>Amount of dates:</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="212"/>
+        <source>Information</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="213"/>
+        <source>Diese Anwendung wurde von Boris Pohler und Gabriel Böhme entwickelt und sie steht unter der GPL3 Lizenz.</source>
+        <translation>This application were developed by Boris Pohler and Gabriel Böhme. It&apos;s released under the conditions of the GPL3 license.</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="223"/>
+        <source>Achtung!</source>
+        <translation>Warning!</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="224"/>
+        <source>Möchtest du den Termin-Feed wirklich beenden?</source>
+        <translation>Do you really want to stop the dates feed?</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="225"/>
+        <source>Ja</source>
+        <translation>Yes</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="226"/>
+        <source>Nein</source>
+        <translation>No</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="239"/>
+        <source>Warnung!</source>
+        <translation>Warning!</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="240"/>
+        <source>Im Moment ist kein Kalender ausgewählt, der angezeigt werden kann.</source>
+        <translation>At the moment, there is no selected calendar, which could be shown.</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="241"/>
+        <source>Jetzt wählen</source>
+        <translation>Select now</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="242"/>
+        <source>Abbrechen</source>
+        <translation>Cancel</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="9"/>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="10"/>
+        <source>manuelles Update</source>
+        <translation>Update manually</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="14"/>
+        <source>Daemon stoppen</source>
+        <translation>Stop daemon</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="15"/>
+        <source>Daemon starten</source>
+        <translation>Start daemon</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="49"/>
+        <source>Feed beenden</source>
+        <translation>Stop the feed</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="50"/>
+        <source>Information</source>
+        <translation>Information</translation>
+    </message>
+</context>
+</TS>
