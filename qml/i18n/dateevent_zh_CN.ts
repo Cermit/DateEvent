a63e020bc0c11ef378de555dac3ae4a3a9dafed3
Gabriel Böhme <m.gabrielboehme@googlemail.com>
2011-11-18 11:59:11 +0100

    Added a community chinese translation file

---
+++ qml/i18n/dateevent_zh_CN.ts
@@ -0,0 +1,158 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en">
+<context>
+    <name>主页</name>
+    <message>
+        <location filename="../MainPage.qml" line="21"/>
+        <source>DateEvent</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="45"/>
+        <source>berücksichtigte Kalendertage:</source>
+        <translation>日历有效范围：</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="67"/>
+        <source>berücksichtigte Kalender:</source>
+        <translation>日历有效范围：</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="74"/>
+        <source>Auswahl Kalender</source>
+        <translation>日历选择</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="90"/>
+        <source>angezeigte Termine:</source>
+        <translation>显示条目：</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="112"/>
+        <source>Nächste Termine zuerst zeigen:</source>
+        <translation>将下一事件置顶：</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="130"/>
+        <source>Daemon starten</source>
+        <translation>启动守护进程</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="141"/>
+        <location filename="../MainPage.qml" line="145"/>
+        <location filename="../MainPage.qml" line="229"/>
+        <source>Start</source>
+        <translation>开始</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="145"/>
+        <source>manuelles Update</source>
+        <translation>手动更新</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="152"/>
+        <source>Kalender Auswahl:</source>
+        <translation>日历选择：</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="154"/>
+        <location filename="../MainPage.qml" line="214"/>
+        <source>Ok</source>
+        <translation>确定</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="168"/>
+        <source>Anzahl Tage:</source>
+        <translation>选择有效天数：</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="191"/>
+        <source>Anzahl Termine:</source>
+        <translation>显示条目：</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="212"/>
+        <source>Information</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="213"/>
+        <source>Diese Anwendung wurde von Boris Pohler und Gabriel Böhme entwickelt und sie steht unter der GPL3 Lizenz.</source>
+        <translation>This application were developed by Boris Pohler and Gabriel Böhme. It&apos;s released under the conditions of the GPL3 license.</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="223"/>
+        <source>Achtung!</source>
+        <translation>警告!</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="224"/>
+        <source>Möchtest du den Termin-Feed wirklich beenden?</source>
+        <translation>您真的想要停止日期馈送？</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="225"/>
+        <source>Ja</source>
+        <translation>是</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="226"/>
+        <source>Nein</source>
+        <translation>否</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="239"/>
+        <source>Warnung!</source>
+        <translation>警告！</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="240"/>
+        <source>Im Moment ist kein Kalender ausgewählt, der angezeigt werden kann.</source>
+        <translation>现在还没有可以显示的日历。</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="241"/>
+        <source>Jetzt wählen</source>
+        <translation>现在选择</translation>
+    </message>
+    <message>
+        <location filename="../MainPage.qml" line="242"/>
+        <source>Abbrechen</source>
+        <translation>取消</translation>
+    </message>
+</context>
+<context>
+    <name>主页</name>
+    <message>
+        <location filename="../main.qml" line="9"/>
+        <source>Start</source>
+        <translation>开始</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="10"/>
+        <source>manuelles Update</source>
+        <translation>手动更新</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="14"/>
+        <source>Daemon stoppen</source>
+        <translation>停止守护进程</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="15"/>
+        <source>Daemon starten</source>
+        <translation>启动守护进程</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="49"/>
+        <source>Feed beenden</source>
+        <translation>停止馈送</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="50"/>
+        <source>Information</source>
+        <translation>关于</translation>
+    </message>
+</context>
+</TS>
